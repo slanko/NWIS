9bf62f592091db658023aad3a304f2571684c21a
lawrence <lawrence.parry@gmail.com>
2020-03-09 11:54:34 +1300

    Broken Track Gen

---
+++ Assets/TrackGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackGenerator : MonoBehaviour
+{
+    public int iterations = 20;
+    public TrackSection[] sections;
+    GameObject origin;
+    void Start()
+    {
+        origin = gameObject;   
+        for(int i = iterations; i >0; i--)
+        {
+            for(int reattempt = 8; reattempt > 0; reattempt--) //Need to put in alternative if fails every time;
+            {
+                int pick = Random.Range(0, sections.Length);
+                GameObject oldOrigin = origin;
+                origin = sections[pick].CreateSection(origin);
+                if (origin != oldOrigin)
+                    continue;
+            }
+            
+        }
+    }
+}
---
+++ Assets/TrackSection.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class TrackSection: ScriptableObject
+{
+    public GameObject prefab;
+    public float length;
+    public float difficulty;
+
+    public GameObject CreateSection(GameObject origin)
+    {
+        GameObject g = Instantiate(prefab, origin.transform.position, origin.transform.rotation);
+        Transform end = g.transform.GetChild(1);
+        Transform start = g.transform.GetChild(0);
+        float distance = Vector3.Distance(start.position, end.position)/2;
+
+        Rigidbody rb = g.AddComponent<Rigidbody>();
+        rb.isKinematic = true;
+        RaycastHit hit;
+        rb.SweepTest(Vector3.zero, out hit);
+        if (hit.collider != null)
+        {
+            Destroy(g);
+            return origin; //This is returned if the track would overlap
+        }
+        else
+        {
+            Destroy(rb);
+            return end.gameObject;
+        }
+    }
+}
