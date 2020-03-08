e47b8556f47ed746c41493c0a8938b868995ac7a
slinkee <QMBFXSL@gmail.com>
2020-03-09 11:48:29 +1300

    really basic control
    auaauhauhghh

--- Assets/Scripts/ShipControl.cs
+++ Assets/Scripts/ShipControl.cs
@@ -5,17 +5,22 @@ using UnityEngine.InputSystem;
 
 public class ShipControl : MonoBehaviour
 {
-
+    Rigidbody rb;
+    public float floatHeight, floatForce;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //ground check
+        if(Physics.Raycast(transform.position, new Vector3(0, -1, 0), floatHeight))
+        {
+            rb.AddForce(Vector3.up * floatForce, ForceMode.Force);
+        }
     }
 }
---
+++ Assets/Scripts/ShipControl2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipControl2 : MonoBehaviour
+{
+    Rigidbody rb;
+    public float accelSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.forward * accelSpeed, ForceMode.Force);
+        }
+    }
+}
