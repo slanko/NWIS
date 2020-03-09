fc77f6e46bf5445a028745097d32e1efd6e4f82b
slink <QMBFXSL@gmail.com>
2020-03-10 04:00:49 +1300

    really basic control but it works
    eee

--- Assets/Scripts/ShipControl.cs
+++ Assets/Scripts/ShipControl.cs
@@ -1,26 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class ShipControl : MonoBehaviour
 {
     Rigidbody rb;
-    public float floatHeight, floatForce;
-
+    public float accelSpeed, turnSpeed;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        //ground check
-        if(Physics.Raycast(transform.position, new Vector3(0, -1, 0), floatHeight))
+        if (Input.GetAxis("Accelerate") != 0)
         {
-            rb.AddForce(Vector3.up * floatForce, ForceMode.Force);
+            rb.AddForce(transform.forward * (Input.GetAxis("Accelerate") * accelSpeed), ForceMode.Force);
         }
+        rb.AddTorque(transform.up * (turnSpeed * Input.GetAxis("Horizontal")));
+
     }
 }
---
+++ Assets/Scripts/floaterScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class floaterScript : MonoBehaviour
+{
+    Rigidbody rb;
+    public float floatHeight, floatForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ground check
+        if(Physics.Raycast(transform.position, new Vector3(0, -1, 0), floatHeight))
+        {
+            rb.AddForce(transform.up * floatForce, ForceMode.Force);
+        }
+    }
+}
