09446d8a758d251e2d7a7cde6490aab34ca48e5f
slinkee <QMBFXSL@gmail.com>
2020-03-11 11:43:56 +1300

    gameplay tweaks (drag stuff)
    and splitscreen dividers

--- Assets/Scripts/ShipControl.cs
+++ Assets/Scripts/ShipControl.cs
@@ -6,7 +6,7 @@ public class ShipControl : MonoBehaviour
 {
     public int playerNum;
     Rigidbody rb;
-    public float accelSpeed, turnSpeed, brakeDrag;
+    public float accelSpeedBase, accelSpeedTweaked, turnSpeed, brakeDrag, xDragDiv;
     public string accelName, horizontalName;
     // Start is called before the first frame update
     void Start()
@@ -20,7 +20,7 @@ public class ShipControl : MonoBehaviour
     {
         if (Input.GetAxis(accelName) > 0)
         {
-            rb.AddForce(transform.forward * (Input.GetAxis(accelName) * accelSpeed), ForceMode.Force);
+            rb.AddForce(transform.forward * (Input.GetAxis(accelName) * accelSpeedTweaked), ForceMode.Force);
             
         }
         if (Input.GetAxis(accelName) < 0)
@@ -32,6 +32,21 @@ public class ShipControl : MonoBehaviour
             rb.drag = 1;
         }
         rb.AddTorque(transform.up * (turnSpeed * Input.GetAxis(horizontalName)));
+
+        //sideways drag stuff
+        var vel = rb.velocity;
+        var velFlat = new Vector3(vel.x, 0, vel.z).normalized;
+        var forward = rb.transform.forward;
+        var angle = Vector3.Angle(forward, velFlat);
+        if(angle > 10)
+        {
+            rb.drag = angle / xDragDiv;
+            if(rb.drag < 1f)
+            {
+                rb.drag = 1;
+            }
+            accelSpeedTweaked = accelSpeedBase + angle;
+        }
 
     }
 }
