be0048fe1f77836dd7cf27eccc632158aecd564a
slink <QMBFXSL@gmail.com>
2020-04-08 09:57:11 +1200

    new skybox, UI system rework

---
+++ Assets/Scripts/CamResize.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamResize : MonoBehaviour
+{
+    public Camera mainCam;
+    Camera meCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        meCam = gameObject.GetComponent<Camera>();
+        meCam.rect = mainCam.rect;
+    }
+}
