1aa5fb4283e19df48f501ae2e12f4291243aeb4b
slink <QMBFXSL@gmail.com>
2020-04-09 14:57:17 +1200

    explosions, missile rework

---
+++ Assets/Scripts/Gameplay/ExplosionScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionScript : MonoBehaviour
+{
+
+    public void DestroyMe()
+    {
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Gameplay/MineScript.cs
+++ Assets/Scripts/Gameplay/MineScript.cs
@@ -7,6 +7,7 @@ public class MineScript : MonoBehaviour
     Rigidbody rb;
     weaponSystem wS;
     public float explosionPower,  explosionRadius;
+    public GameObject explosion;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +24,7 @@ public class MineScript : MonoBehaviour
             rb.AddExplosionForce(explosionPower, transform.position, explosionRadius, 1f, ForceMode.Impulse);
             //rb.AddForceAtPosition(Vector3.up * explosionPower, transform.position, ForceMode.Impulse);
             wS.health = wS.health - 1;
+            Instantiate(explosion, transform.position, transform.rotation);
             Destroy(this.gameObject);
         }
 
