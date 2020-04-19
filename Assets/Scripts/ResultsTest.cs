5f8e1f64fc1426f0f3c013bcabf20b3ae749b65b
lawrence <lawrence.parry@gmail.com>
2020-04-20 10:01:40 +1200

    Results Screen Fixes. Tweaks

---
+++ Assets/Scripts/ResultsTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultsTest : MonoBehaviour
+{
+    [SerializeField] Player[] players;
+    private void Awake()
+    {
+        for(int i =0; i < players.Length; i++)
+        {
+            Placings.placings[i] = players[i];
+        }
+    }
+}
