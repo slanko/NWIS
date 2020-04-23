3a27cef4ba67f5f19c712f34e22aa70f63a2245f
lawrence <lawrence.parry@gmail.com>
2020-04-23 12:23:27 +1200

    Fixed Reset Bug

--- Assets/Scripts/Gameplay/ShipControl.cs
+++ Assets/Scripts/Gameplay/ShipControl.cs
@@ -6,23 +6,42 @@ public class ShipControl : MonoBehaviour
 {
     public int playerNum;
     Rigidbody rb;
-    public float accelSpeed, turnSpeed, brakeDrag;
-    public string accelName, horizontalName;
+    public float accelSpeedBase, accelSpeedTweaked, turnSpeedBase, tweakedTurnSpeed, turnSpeedDecrease, minTurnSpeed, brakeDrag, xDragDiv, correctForce, controlHeight, baseCamFOV, downForce;
     public Vector3 localVelocity;
+    public string accelName, horizontalName;
+    public Camera myCam;
+    bool raceStarted = false;
+    RaceStateEvents raceState;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         accelName = playerNum + "PAccelerate";
         horizontalName = playerNum + "PHorizontal";
+        baseCamFOV = myCam.fieldOfView;
+        raceState = GameObject.FindGameObjectWithTag("RaceState").GetComponent<RaceStateEvents>();
+        raceState.beginRace.AddListener(RaceStart);
     }
 
-    private void Update()
+    public void RaceStart()
     {
+        raceStarted = true;
+    }
+    private void OnDestroy()
+    {
+        raceState.beginRace.RemoveListener(RaceStart);
+    }
+
+    private void FixedUpdate()
+    {
+        //input/acceleration and turning script
         if (Input.GetAxis(accelName) > 0)
         {
-            rb.AddForce(transform.forward * (Input.GetAxis(accelName) * accelSpeed), ForceMode.Force);
-            
+            if (raceStarted == true)
+            {
+                rb.AddForce(transform.forward * (Input.GetAxis(accelName) * accelSpeedBase), ForceMode.Force);
+            }
+
         }
         if (Input.GetAxis(accelName) < 0)
         {
@@ -32,7 +51,49 @@ public class ShipControl : MonoBehaviour
         {
             rb.drag = 1;
         }
-        rb.AddTorque(transform.up * (turnSpeed * Input.GetAxis(horizontalName)));
-        localVelocity = transform.TransformVector(rb.velocity);
+
+        //changing x velocity to make it turn better
+        var localVel = transform.InverseTransformDirection(rb.velocity);
+
+        localVel.x = localVel.x / xDragDiv;
+        if (localVel.y > 0)
+        {
+            localVel.y = localVel.y / 1.5f;
+        }
+
+        //tweakedTurnSpeed = turnSpeedBase - localVel.z;
+        tweakedTurnSpeed = turnSpeedBase - Mathf.Pow(localVel.z * turnSpeedDecrease, 2);
+        if (tweakedTurnSpeed < minTurnSpeed)
+        {
+            tweakedTurnSpeed = minTurnSpeed;
+        }
+
+        if (raceStarted == true)
+        {
+            rb.AddTorque(transform.up * (tweakedTurnSpeed * Input.GetAxis(horizontalName)));
+        }
+
+
+        localVelocity = new Vector3(localVel.x, localVel.y, localVel.z);
+        rb.velocity = transform.TransformDirection(localVel);
+
+        //FOV bullshit
+        myCam.fieldOfView = baseCamFOV + (localVel.z / 3f);
+
+
+
+        if (Physics.Raycast(transform.position, Vector3.down, out var hit, controlHeight))
+        {
+            var up = hit.normal;
+            var localUp = rb.transform.up;
+
+            // torque localUp toward up
+            rb.AddForceAtPosition(up * Time.deltaTime, localUp * correctForce, ForceMode.Impulse);
+            rb.AddForceAtPosition(-up * Time.deltaTime, localUp * -correctForce, ForceMode.Impulse);
+        }
+
+        //downforce script
+        rb.AddForce(Vector3.down * downForce);
+
     }
 }
