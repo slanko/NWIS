f452900d7e88c5b3cef438da4f442a3378cec86b
slink <QMBFXSL@gmail.com>
2020-04-28 09:59:41 +1200

    more tweaks
    aegh

--- Assets/Scripts/Gameplay/ExplosionScript.cs
+++ Assets/Scripts/Gameplay/ExplosionScript.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class ExplosionScript : MonoBehaviour
 {
+    public GameObject particlez; 
 
     public void DestroyMe()
     {
+        particlez.transform.parent = null;
         Destroy(gameObject);
+
     }
 }
