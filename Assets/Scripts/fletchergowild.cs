d1ce0d6d470cf2794088c31ee0edd548a1cc7567
slink <QMBFXSL@gmail.com>
2020-05-02 04:50:11 +1200

    tweaks n changes
    i'm scared i'm gonna goof up right at the last second so here's a commit

---
+++ Assets/Scripts/fletchergowild.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fletchergowild : MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.speed = Random.Range(0.9f, 5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
